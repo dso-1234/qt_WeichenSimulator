4a8580fe1eb7e89ad104815716b41773a94f895b
DSO <39367483+dso-1234@users.noreply.github.com>
2025-04-14 13:29:29 +0200

    Implement basic login system with username/password authentication
    Implement basic login system with username/password authentication

--- Proj_BC01_Simulator/mainwindow.cpp
+++ Proj_BC01_Simulator/mainwindow.cpp
@@ -1,18 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "../BC01_GENERIC_POINT/BC01_GENERIC_POINT.h"
-#include "../BC02_POINT_TYPA/BC02_POINT_TYPA.h"
-
-
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    /*
+    connect(ui->loginButton, &QPushButton::clicked, this, &MainWindow::checkLogin);
+}
 
-    */
+void MainWindow::checkLogin() {
+    QString username = ui->usernameLineEdit->text();
+    QString password = ui->passwordLineEdit->text();
+
+    if (username == "admin" && password == "secret") {
+        QMessageBox::information(this, "Success", "Login successful!");
+    } else {
+        QMessageBox::warning(this, "Error", "Invalid username or password");
+    }
 }
 
 MainWindow::~MainWindow()
